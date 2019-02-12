8776dea5feceb962eb936cfb2bcd81832f4ad300
Paul Williams <williamspaul@google.com>
2019-02-11 23:29:03 -0700

    update docs and copyright notices

--- src/main/java/com/google/apigee/demo/SimpleService.java
+++ src/main/java/com/google/apigee/demo/SimpleService.java
@@ -1,23 +1,19 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements. See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership. The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
 package com.google.apigee.demo;
+/*
+        Copyright 2019 Google LLC
+
+        Licensed under the Apache License, Version 2.0 (the "License");
+        you may not use this file except in compliance with the License.
+        You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+        Unless required by applicable law or agreed to in writing, software
+        distributed under the License is distributed on an "AS IS" BASIS,
+        WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+        See the License for the specific language governing permissions and
+        limitations under the License.
+*/
 
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
@@ -27,6 +23,11 @@ import java.util.Set;
 import java.util.TreeSet;
 import java.util.stream.Collectors;
 
+/**
+ * Just a basic service to do a few uninteresting things - return lists of resources that are available for reservations
+ * return existing reservations, and propose a new reservation.
+ * All persistence is in memory only -- just restart and you have fresh data.
+ */
 @WebService(serviceName = "SimpleService", targetNamespace = "http://soapy-sample.appspot.com/soapy")
 public class SimpleService {
     private static final ResourcesManager resources = new ResourcesManager();
